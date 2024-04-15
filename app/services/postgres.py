5eeb778bf3e9dd7acab02643013c1bccf8516adf
artem <artem.serdechnyy@gmail.com>
2024-04-16 01:54:42 +0300

    test export_requirements.yaml | linters.yaml

--- app/services/postgres.py
+++ app/services/postgres.py
@@ -11,6 +11,10 @@ from sqlalchemy.pool import NullPool
 
 class _PostgresConfig(BaseSettings):
     POSTGRES_SERVER: str = "localhost"
+
+
+
+
     POSTGRES_PORT: int = 5432
     POSTGRES_DB: str = "postgres"
     POSTGRES_USER: str = "postgres"
