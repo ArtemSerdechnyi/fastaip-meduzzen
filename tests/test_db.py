8d1da9e15f4e28b4f52ea464fdf71cd3e53f6378
artem <artem.serdechnyy@gmail.com>
2024-04-15 11:37:13 +0300

    add redis and postgres to compose file, add connections

---
+++ tests/test_db.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.services import get_async_session
+
+
+@pytest.mark.anyio
+async def test_postgres_db_connection():
+    async for session in get_async_session():
+        assert isinstance(session, AsyncSession)
+        result = await session.execute(text("SELECT 1;"))
+        value = result.scalar_one()
+        assert value == 1
