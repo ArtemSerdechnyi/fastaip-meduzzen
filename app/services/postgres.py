c2dd41d0820ab5e9a77903835dffdf8a94b353b7
artem <artem.serdechnyy@gmail.com>
2024-04-18 13:10:11 +0300

    test CI/CD

--- app/services/postgres.py
+++ app/services/postgres.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     create_async_engine,
 )
-from sqlalchemy.orm import DeclarativeBase, declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
 from sqlalchemy.pool import NullPool
 
