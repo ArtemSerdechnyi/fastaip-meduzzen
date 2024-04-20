60284eff892b540b69e65058ba7d6c3fbddad017
artem <artem.serdechnyy@gmail.com>
2024-04-20 15:25:32 +0300

    add tests for user service

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,20 +3,19 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.postgres import get_async_session
+from .conftest import override_get_async_session
 
 
-@pytest.mark.anyio
-async def test_postgres_db_connection():
-    async for session in get_async_session():
+async def test_postgres_db_connection(session=get_async_session):
+    async for session in session():
         assert isinstance(session, AsyncSession)
         result = await session.execute(text("SELECT 1;"))
         value = result.scalar_one()
         assert value == 1
 
 
-@pytest.mark.anyio
-async def test_check_user_table_exist():
-    async for session in get_async_session():
+async def test_check_user_table_exist(session=get_async_session):
+    async for session in session():
         assert isinstance(session, AsyncSession)
         query = text(
             "SELECT EXISTS (SELECT FROM pg_tables "
@@ -26,3 +25,11 @@ async def test_check_user_table_exist():
         result = await session.execute(query)
         exists = result.scalar_one()
         assert exists is True
+
+
+async def test_postgres_test_db_connection():
+    await test_postgres_db_connection(override_get_async_session)
+
+
+async def test_check_user_table_test_db_exist():
+    await test_check_user_table_exist(override_get_async_session)
