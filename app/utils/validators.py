612d1e9d7c6c9766546fe34c71b54676c4f7c5b7
artem <artem.serdechnyy@gmail.com>
2024-04-30 16:57:25 +0300

    add dockstring to validators

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -26,6 +26,12 @@ class BaseValidator(ABC):
 
 class UserValidator(BaseValidator):
     def validate_exist_company_is_active(self, func):
+        """
+        Check company is exist and active.
+
+        Depends: company_id
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             company_id: UUID = kwargs["company_id"]
@@ -47,6 +53,12 @@ class UserValidator(BaseValidator):
         return wrapper
 
     def validate_request_id_belongs_to_user(self, func):
+        """
+        Check request_id belongs to user.
+
+        Depends: request_id, user
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             company_id: UUID = kwargs["company_id"]
@@ -69,6 +81,12 @@ class UserValidator(BaseValidator):
         return wrapper
 
     def validate_user_leave_from_company(self, func):
+        """
+        Validate user is active member of the specified company.
+
+        Depends: company_id, user
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             company_id: UUID = kwargs["company_id"]
@@ -99,6 +117,14 @@ class UserValidator(BaseValidator):
         return wrapper
 
     def validate_company_invitation(self, func):
+        """
+        Validate the company invitation  for a user.
+        Check user and company is exist.
+        Check user not a member of the company.
+
+        Depends: request_id, user
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             request_id: UUID = kwargs["request_id"]
@@ -113,8 +139,7 @@ class UserValidator(BaseValidator):
             query = self._build_where_exist_select_query(
                 CompanyRequest.request_id == request_id,
                 CompanyRequest.user_id == user.user_id,
-                CompanyRequest.status
-                == CompanyRequestStatus.pending.value,
+                CompanyRequest.status == CompanyRequestStatus.pending.value,
                 CompanyRequest.is_active == True,
                 Company.is_active == True,
                 Company.owner_id != CompanyRequest.user_id,
@@ -133,6 +158,12 @@ class UserValidator(BaseValidator):
         return wrapper
 
     def validate_user_request_non_existing(self, func):
+        """
+        Validate that a user request does not already exist base on attributes.
+
+        Depends: user, company_id
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             user: User = kwargs["user"]
@@ -161,6 +192,12 @@ class UserValidator(BaseValidator):
         return wrapper
 
     def validate_check_user_not_in_company(self, func):
+        """
+        Validate that the user is not a member of the company.
+
+        Depends: company_id, user
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             company_id: UUID = kwargs["company_id"]
@@ -186,6 +223,12 @@ class UserValidator(BaseValidator):
 
 class CompanyValidator(BaseValidator):
     def validate_company_id_by_owner(self, func):
+        """
+        Validate that the user is the owner of the company.
+
+        Depends: company_id, owner
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             company_id: UUID = kwargs["company_id"]
@@ -209,6 +252,12 @@ class CompanyValidator(BaseValidator):
         return wrapper
 
     def validate_company_owner_by_company_request_id(self, func):
+        """
+        Validate that the user is the owner of the company base on given company request.
+
+        Depends: request_id, owner
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             company_request_id: UUID = kwargs["request_id"]
@@ -235,6 +284,12 @@ class CompanyValidator(BaseValidator):
         return wrapper
 
     def validate_company_owner_by_user_request_id(self, func):
+        """
+        Validate that the user is the owner of the company base on given user request.
+
+        Depends: request_id, owner
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             user_request_id: UUID = kwargs["request_id"]
@@ -261,6 +316,12 @@ class CompanyValidator(BaseValidator):
         return wrapper
 
     def validate_user_exist_and_active_by_user_id(self, func):
+        """
+        Validate that the user exists and is active based on user_id.
+
+        Depends: user_id
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             user_id: UUID = kwargs["user_id"]
@@ -283,6 +344,12 @@ class CompanyValidator(BaseValidator):
         return wrapper
 
     def validate_user_request_by_request_id(self, func):
+        """
+        Validate the user request based on the provided request_id.
+
+        Depends: request_id
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             user_request_id: UUID = kwargs["request_id"]
@@ -310,6 +377,12 @@ class CompanyValidator(BaseValidator):
         return wrapper
 
     def validate_company_request_non_existing(self, func):
+        """
+        Validate company request does not already exist base on user_id and company_id.
+
+        Depends: user_id, company_id
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             user_id: UUID = kwargs["user_id"]
@@ -337,6 +410,12 @@ class CompanyValidator(BaseValidator):
         return wrapper
 
     def validate_check_user_not_in_company(self, func):
+        """
+        Validate that the user is not a member of the given company.
+
+        Depends: company_id, user_id
+        """
+
         @wraps(func)
         async def wrapper(self_service, **kwargs):
             company_id: UUID = kwargs["company_id"]
