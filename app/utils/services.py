6a11ccee2fdf5e75a495028d9cdec1f5bb839333
artem <artem.serdechnyy@gmail.com>
2024-05-03 12:19:53 +0300

    update utils services

--- app/utils/services.py
+++ app/utils/services.py
@@ -5,6 +5,7 @@ from app.db.postgres import get_async_session
 from app.services.company import CompanyService
 from app.services.user import UserService
 from app.services.user_action import UserActionService
+from app.services.company_action import CompanyActionService
 
 
 async def get_user_service(db: AsyncSession = Depends(get_async_session)):
@@ -27,7 +28,5 @@ async def get_user_action_service(
 async def get_company_action_service(
     db: AsyncSession = Depends(get_async_session),
 ):
-    from app.services.company_action import CompanyActionService
-
     async with CompanyActionService(session=db) as service:
         yield service
